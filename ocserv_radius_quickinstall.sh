4f8ef01716ff5a240022642e052f95127a906d99
Abdullah Sajid <mehar.abdullah13@gmail.com>
2019-09-18 20:55:55 +0500

    Fixed a bug

--- ocserv_radius_quickinstall.sh
+++ ocserv_radius_quickinstall.sh
@@ -2,5 +2,5 @@ sudo -i
 yum install wget -y 
 wget https://raw.githubusercontent.com/codescope/ocserv/master/radius_for_ocserv.sh
 chmod +x radius_for_ocserv.sh
-./radius_for_ocserv.sh
+sh ./radius_for_ocserv.sh
 #
