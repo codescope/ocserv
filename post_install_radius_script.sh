7cc6378e81f5bc65e0ada8f84fea6863f0282ce0
Abdullah Sajid <mehar.abdullah13@gmail.com>
2019-09-23 21:18:18 +0500

    fixed a bug

--- post_install_radius_script.sh
+++ post_install_radius_script.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-sudo -i
 sed -i '/auth = "plain\[passwd=\/etc\/ocserv\/ocpasswd\]"/s/^/#/g' /etc/ocserv/ocserv.conf
 sed -i '/^#auth = "radius\[config=\/etc\/radiusclient\-ng\/radiusclient\.conf,groupconfig=true\]"/s/^#//g' /etc/ocserv/ocserv.conf
 sed -i '/^#acct = "radius\[config=\/etc\/radiusclient\-ng\/radiusclient\.conf\]"/s/^#//g' /etc/ocserv/ocserv.conf
 systemctl restart ocserv
-sleep 3
-exit
-#
+sleep 2
