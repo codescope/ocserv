e925b1eab2813d2143de249c02a48ff06408dfae
Abdullah Sajid <mehar.abdullah13@gmail.com>
2019-09-23 20:17:42 +0500

    Added post script

---
+++ post_install_radius_script.sh
@@ -0,0 +1,5 @@
+sudo -i
+sed -i '/auth = "plain\[passwd=\/etc\/ocserv\/ocpasswd\]"/s/^/#/g' /etc/ocserv/ocserv.conf
+sed -i '/^#auth = "radius\[config=\/etc\/radiusclient\-ng\/radiusclient\.conf,groupconfig=true\]"/s/^#//g' /etc/ocserv/ocserv.conf
+sed -i '/^#acct = "radius\[config=\/etc\/radiusclient\-ng\/radiusclient\.conf\]"/s/^#//g' /etc/ocserv/ocserv.conf
+systemctl restart ocserv
